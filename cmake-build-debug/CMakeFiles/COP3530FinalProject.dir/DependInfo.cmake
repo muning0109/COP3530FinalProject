3cf8997a130ef08ec64f33d69d66e4f6ac6ab846
muning0109 <73597461+muning0109@users.noreply.github.com>
2021-12-07 10:05:39 -0500

    xx

--- cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/ke/CLionProjects/COP3530FinalProject/SFML_Process.cpp" "/Users/ke/CLionProjects/COP3530FinalProject/cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/SFML_Process.cpp.o"
-  "/Users/ke/CLionProjects/COP3530FinalProject/main.cpp" "/Users/ke/CLionProjects/COP3530FinalProject/cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/main.cpp.o"
+  "/Users/muningchen/Desktop/COP3530FinalProject/SFML_Process.cpp" "/Users/muningchen/Desktop/COP3530FinalProject/cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/SFML_Process.cpp.o"
+  "/Users/muningchen/Desktop/COP3530FinalProject/main.cpp" "/Users/muningchen/Desktop/COP3530FinalProject/cmake-build-debug/CMakeFiles/COP3530FinalProject.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
